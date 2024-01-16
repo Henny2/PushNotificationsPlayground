80c6b92d042d5ba6e39dc94ec6b0cd073a4348a4
Henny2 <henrieke.baunack@web.de>
2024-01-15 19:06:05 -0800

    Initial Commit

---
+++ PushNotificationsPlayground/PushNotificationsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationsPlaygroundApp.swift
+//  PushNotificationsPlayground
+//
+//  Created by Henrieke Baunack on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
