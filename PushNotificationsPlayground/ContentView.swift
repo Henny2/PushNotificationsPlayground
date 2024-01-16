56b93f8a1f74aa96778d77fb83b4a1f4a74da1b8
Henny2 <henrieke.baunack@web.de>
2024-01-15 19:29:48 -0800

    adding source for tutorial

--- PushNotificationsPlayground/ContentView.swift
+++ PushNotificationsPlayground/ContentView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+import UserNotifications
+
+// source: https://www.youtube.com/watch?v=XnnDHDlPwLw
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Button("Request permission"){
+                // requesting authorization to send notifications
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("we are all set")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            Button("Schedule notification"){
+                // make content
+                let content  = UNMutableNotificationContent()
+                content.title = "Feed the dogs"
+                content.subtitle = "They are hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
         }
-        .padding()
     }
 }
 
